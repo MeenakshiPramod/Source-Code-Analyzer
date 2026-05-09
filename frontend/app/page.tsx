55ef1f83dd2250c19d64045f53b7d0a45fafe976
Meenakshi Pramod <meenakshipramod04@gmail.com>
2026-05-09 11:02:18 +0530

    Frontend module started

---
+++ frontend/app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+
+      <div className="text-center">
+
+        <h1 className="text-5xl font-bold mb-6">
+          Realtime Source Code Analyzer 🚀
+        </h1>
+
+        <p className="text-gray-400 text-lg">
+          AI-powered GitHub repository analysis
+        </p>
+
+      </div>
+
+    </main>
+  );
+}
